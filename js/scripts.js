807d071cf134df2dc166d3e9fa4c95cf33f9a45f
teruo-ot <marcos.teruo.ouchi@gmail.com>
2021-11-01 19:53:55 -0300

    Atualização do JQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
 // Função que permite o Include de html no html
+// Solução apresentada em https://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
 
 $(function () {
     var includes = $('[data-include]')
